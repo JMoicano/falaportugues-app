f18b5229f9053b6ceec00fbf82831aa473783b83
João Mário Soares Silva <jmariossilva@gmail.com>
2023-09-04 22:50:02 -0300

    landing page without style

--- src/App.js
+++ src/App.js
@@ -1,25 +1,62 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState, useCallback, useEffect } from 'react';
+const FALA_PORTUGUES_API_URL = "http://localhost:4000/api"
 
 function App() {
+
+  const [noun, setNoun] = useState(null)
+  const [adjective, setAdjective] = useState(null)
+
+  const fetchNoun = useCallback(async () => {
+    const nounResponse = await fetch(`${FALA_PORTUGUES_API_URL}/trend-noun`)
+    const selectedNoun = await nounResponse.json()
+    setNoun(selectedNoun[0])
+  }, [])
+
+  const fetchAdjective = useCallback(async () => {
+    const adjectiveResponse = await fetch(`${FALA_PORTUGUES_API_URL}/trend-adjective`)
+    const selectedAdjective = await adjectiveResponse.json()
+    setAdjective(selectedAdjective[0])
+  }, [])
+
+  useEffect(() => {
+    fetchNoun()
+  }, [fetchNoun]) 
+
+  useEffect(() => {
+    fetchAdjective()
+  }, [fetchAdjective]) 
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <body>
+        <div>
+          <WordContainer word={adjective}/>
+          <WordContainer word={noun}/>
+        </div>
+        <div>
+          <UpdateButton buttonLabel="Update Adjective" onUpdateRequested={fetchAdjective}/>
+          <UpdateButton buttonLabel="Update Noun" onUpdateRequested={fetchNoun}/>
+          <UpdateButton buttonLabel="New trend" onUpdateRequested={() => {
+            fetchNoun(); 
+            fetchAdjective()
+          }}/>
+        </div>
+      </body>
     </div>
   );
+}
+
+function WordContainer({ word }) {
+  return (
+    <h1>{word}</h1>
+  )
+}
+
+function UpdateButton({ buttonLabel, onUpdateRequested }) {
+  return (
+    <button onClick={onUpdateRequested}>{buttonLabel}</button>
+  )
 }
 
 export default App;
