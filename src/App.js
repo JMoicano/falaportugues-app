3397bd5ae5bc4b193a81a3fc720e55ed53849711
Savio Serra <serra.santos.s@gmail.com>
2023-09-04 23:15:24 -0300

    chore: install tailwindcss

--- src/App.js
+++ src/App.js
@@ -1,46 +1,58 @@
-import './App.css';
-import { useState, useCallback, useEffect } from 'react';
-const FALA_PORTUGUES_API_URL = "http://localhost:4000/api"
+import "./App.css";
+import { useState, useCallback, useEffect } from "react";
+
+const FALA_PORTUGUES_API_URL = "http://localhost:4000/api";
 
 function App() {
-
-  const [noun, setNoun] = useState(null)
-  const [adjective, setAdjective] = useState(null)
+  const [noun, setNoun] = useState(null);
+  const [adjective, setAdjective] = useState(null);
 
   const fetchNoun = useCallback(async () => {
-    const nounResponse = await fetch(`${FALA_PORTUGUES_API_URL}/trend-noun`)
-    const selectedNoun = await nounResponse.json()
-    setNoun(selectedNoun[0])
-  }, [])
+    const nounResponse = await fetch(`${FALA_PORTUGUES_API_URL}/trend-noun`);
+    const selectedNoun = await nounResponse.json();
+    setNoun(selectedNoun[0]);
+  }, []);
 
   const fetchAdjective = useCallback(async () => {
-    const adjectiveResponse = await fetch(`${FALA_PORTUGUES_API_URL}/trend-adjective`)
-    const selectedAdjective = await adjectiveResponse.json()
-    setAdjective(selectedAdjective[0])
-  }, [])
+    const adjectiveResponse = await fetch(
+      `${FALA_PORTUGUES_API_URL}/trend-adjective`
+    );
+    const selectedAdjective = await adjectiveResponse.json();
+    setAdjective(selectedAdjective[0]);
+  }, []);
 
   useEffect(() => {
-    fetchNoun()
-  }, [fetchNoun]) 
+    fetchNoun();
+  }, [fetchNoun]);
 
   useEffect(() => {
-    fetchAdjective()
-  }, [fetchAdjective]) 
+    fetchAdjective();
+  }, [fetchAdjective]);
 
   return (
     <div className="App">
       <body>
         <div>
-          <WordContainer word={adjective}/>
-          <WordContainer word={noun}/>
+          <WordContainer word={adjective} />
+          <WordContainer word={noun} />
         </div>
+
         <div>
-          <UpdateButton buttonLabel="Update Adjective" onUpdateRequested={fetchAdjective}/>
-          <UpdateButton buttonLabel="Update Noun" onUpdateRequested={fetchNoun}/>
-          <UpdateButton buttonLabel="New trend" onUpdateRequested={() => {
-            fetchNoun(); 
-            fetchAdjective()
-          }}/>
+          <UpdateButton
+            buttonLabel="Update Adjective"
+            onUpdateRequested={fetchAdjective}
+          />
+          <UpdateButton
+            buttonLabel="Update Noun"
+            onUpdateRequested={fetchNoun}
+          />
+          <UpdateButton
+            buttonLabel="New trend"
+            onUpdateRequested={() => {
+              fetchNoun();
+              fetchAdjective();
+            }}
+          />
         </div>
       </body>
     </div>
@@ -48,15 +60,11 @@ function App() {
 }
 
 function WordContainer({ word }) {
-  return (
-    <h1>{word}</h1>
-  )
+  return <h1>{word}</h1>;
 }
 
 function UpdateButton({ buttonLabel, onUpdateRequested }) {
-  return (
-    <button onClick={onUpdateRequested}>{buttonLabel}</button>
-  )
+  return <button onClick={onUpdateRequested}>{buttonLabel}</button>;
 }
 
 export default App;
