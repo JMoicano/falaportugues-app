e9feebbf4c8192b74e0561b186f13c5d275e77ea
Savio Serra <serra.santos.s@gmail.com>
2023-09-05 22:23:32 -0300

    fix: performance issues

--- src/app/page.js
+++ src/app/page.js
@@ -1,8 +1,8 @@
 "use client";
 
 import classNames from "classnames";
-import { useState, useCallback, useEffect } from "react";
-import { AnimatePresence, motion } from "framer-motion";
+import { motion } from "framer-motion";
+import { useState, useEffect, startTransition } from "react";
 
 import * as apiService from "../api.service";
 import { abril, rajdhani } from "./fonts";
@@ -10,17 +10,36 @@ import { abril, rajdhani } from "./fonts";
 const bufferThresholder = 2;
 
 function App() {
-  const [nounsBuffer, setNoun] = useState([]);
-  const [adjectivesBuffer, setAdjective] = useState([]);
+  const [nounsBuffer, setNounsBuffer] = useState([]);
+  const [adjectivesBuffer, setAdjectivesBuffer] = useState([]);
 
-  const nextNoun = () => setNoun(([, ...prev]) => prev);
-  const nextAdjective = () => setAdjective(([, ...prev]) => prev);
+  const currentNoun = nounsBuffer[0];
+  const currentAdjective = adjectivesBuffer[0];
+
+  const nextNoun = () =>
+    startTransition(() => {
+      setNounsBuffer(([, ...prev]) => prev);
+    });
+
+  const nextAdjective = () =>
+    startTransition(() => {
+      setAdjectivesBuffer(([, ...prev]) => prev);
+    });
+
+  const nextTrend = () => {
+    startTransition(() => {
+      nextNoun();
+      nextAdjective();
+    });
+  };
 
   useEffect(() => {
     if (adjectivesBuffer.length <= bufferThresholder) {
       apiService
         .fetchAdjective()
-        .then((adjectives) => setAdjective((prev) => [...prev, ...adjectives]));
+        .then((adjectives) =>
+          setAdjectivesBuffer((prev) => [...prev, ...adjectives])
+        );
     }
   }, [adjectivesBuffer]);
 
@@ -28,68 +47,56 @@ function App() {
     if (nounsBuffer.length <= bufferThresholder) {
       apiService
         .fetchNoun()
-        .then((nouns) => setNoun((prev) => [...prev, ...nouns]));
+        .then((nouns) => setNounsBuffer((prev) => [...prev, ...nouns]));
     }
   }, [nounsBuffer]);
-
-  const fetchAll = useCallback(async () => {
-    const [nouns, adjectives] = await Promise.all([
-      apiService.fetchNoun(),
-      apiService.fetchAdjective(),
-    ]);
-
-    setNoun(nouns);
-    setAdjective(adjectives);
-  }, []);
-
-  useEffect(() => {
-    fetchAll();
-  }, [fetchAll]);
-
-  const currentNoun = nounsBuffer[0];
-  const currentAdjective = adjectivesBuffer[0];
 
   const isWaitingBuffer =
     nounsBuffer.length <= 1 || adjectivesBuffer.length <= 1;
 
   return (
     <>
-      <span className={classNames("mb-4", rajdhani.className)}>
-        A nova tendência do mercado é:
-      </span>
+      <span className={rajdhani.className}>A nova tendência do mercado é:</span>
 
       <WordContainer>
-        <AnimatePresence mode="popLayout" presenceAffectsLayout>
-          <motion.span
-            layout
-            key={currentAdjective}
-            className="inline-block"
-            animate={{ y: 0, opacity: 1 }}
-            exit={{ y: "20%", opacity: 0 }}
-            initial={{ y: "-20%", opacity: 0 }}
-          >
-            {currentAdjective}
-          </motion.span>
-          <span> </span>
-          <motion.span
-            layout
-            key={currentNoun}
-            className="inline-block"
-            animate={{ y: 0, opacity: 1 }}
-            exit={{ y: "20%", opacity: 0 }}
-            initial={{ y: "-20%", opacity: 0 }}
-          >
-            {currentNoun}
-          </motion.span>
-        </AnimatePresence>
+        <motion.span
+          layout
+          key={currentAdjective}
+          className="inline-block"
+          animate={{ y: 0, opacity: 1 }}
+          exit={{ y: "20%", opacity: 0 }}
+          initial={{ y: "-20%", opacity: 0 }}
+        >
+          {currentAdjective}
+        </motion.span>
+        <span> </span>
+        <motion.span
+          layout
+          key={currentNoun}
+          className="inline-block"
+          animate={{ y: 0, opacity: 1 }}
+          exit={{ y: "20%", opacity: 0 }}
+          initial={{ y: "-20%", opacity: 0 }}
+        >
+          {currentNoun}
+        </motion.span>
       </WordContainer>
 
       <div
         className={classNames(
           { "opacity-5": isWaitingBuffer },
-          "transition-all opacity-100 flex flex-col mt-12 gap-2 lg:gap-6 w-6/12 2xl:w-4/12"
+          "transition-all opacity-100 flex flex-col gap-2 lg:gap-6 w-6/12 2xl:w-4/12"
         )}
       >
+        <Button
+          autoFocus
+          className="flex-1"
+          onClick={() => nextTrend()}
+          disabled={isWaitingBuffer}
+        >
+          Nova trend
+        </Button>
+
         <div className="flex flex-col lg:flex-row gap-2 lg:gap-6">
           <Button
             className="flex-1"
@@ -107,14 +114,6 @@ function App() {
             Novo substantivo
           </Button>
         </div>
-
-        <Button
-          className="flex-1"
-          onClick={() => fetchAll()}
-          disabled={isWaitingBuffer}
-        >
-          Nova trend
-        </Button>
       </div>
     </>
   );
@@ -126,7 +125,7 @@ function WordContainer({ children, className }) {
       className={classNames(
         className,
         abril.className,
-        "text-3xl lg:text-8xl text-center px-4 my-8 lg:py-4 rounded lg:rounded-xl text-white lowercase"
+        "text-3xl lg:text-8xl text-center my-12 lg:my-24 lg:py-4 text-white lowercase mx-6"
       )}
     >
       {children}
@@ -139,7 +138,7 @@ function Button({ children, className, ...props }) {
     <button
       className={classNames(
         className,
-        "transition-all text-white hover:text-slate-900 border-white hover:bg-cyan-400 text-sm lg:text-lg border rounded lg:rounded-lg border-transparent hover:border-cyan-300 px-4 py-2"
+        "transition-all text-white hover:text-slate-900 border-white hover:bg-cyan-400 focus:bg-cyan-400 text-sm lg:text-lg border border-transparent hover:border-cyan-300 px-4 py-2 rounded"
       )}
       {...props}
     >
