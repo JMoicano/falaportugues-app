132b3e46883ac1511d5146409b834c83126cd752
Savio Serra <serra.santos.s@gmail.com>
2023-09-20 01:45:11 -0300

    feat: add share button

--- src/app/page.js
+++ src/app/page.js
@@ -1,5 +1,6 @@
 "use client";
 
+import Head from "next/head";
 import classNames from "classnames";
 import { motion } from "framer-motion";
 import { useState, useEffect, startTransition } from "react";
@@ -33,6 +34,13 @@ function App() {
     });
   };
 
+  const onShare = () => {
+    navigator.share?.({
+      url: "https://falaportugues.vercel.app",
+      text: `A nova tendência de mercado é: ${currentAdjective} ${currentNoun}`,
+    });
+  };
+
   useEffect(() => {
     if (adjectivesBuffer.length <= bufferThresholder) {
       apiService
@@ -56,6 +64,13 @@ function App() {
 
   return (
     <>
+      <Head>
+        <meta
+          property="og:image"
+          content={`https://placehold.co/600x400/EEE/31343C?font=playfair-display&text=${currentAdjective}%20%${currentNoun}`}
+        />
+      </Head>
+
       <span className={rajdhani.className}>A nova tendência do mercado é:</span>
 
       <h1
@@ -93,20 +108,32 @@ function App() {
           "transition-all opacity-100 flex flex-col gap-2 lg:gap-6 w-6/12 2xl:w-4/12"
         )}
       >
-        <Button
-          autoFocus
-          className="flex-1"
-          onClick={() => nextTrend()}
-          disabled={isWaitingBuffer}
-        >
-          Nova trend
-        </Button>
+        <div className="flex gap-2 lg:gap-6">
+          <Button
+            className="flex-1"
+            onClick={nextTrend}
+            disabled={isWaitingBuffer}
+          >
+            Nova trend
+          </Button>
+
+          <Button onClick={onShare} className="border-none">
+            <svg
+              height="1em"
+              fill="white"
+              viewBox="0 0 448 512"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path d="M352 224c53 0 96-43 96-96s-43-96-96-96s-96 43-96 96c0 4 .2 8 .7 11.9l-94.1 47C145.4 170.2 121.9 160 96 160c-53 0-96 43-96 96s43 96 96 96c25.9 0 49.4-10.2 66.6-26.9l94.1 47c-.5 3.9-.7 7.8-.7 11.9c0 53 43 96 96 96s96-43 96-96s-43-96-96-96c-25.9 0-49.4 10.2-66.6 26.9l-94.1-47c.5-3.9 .7-7.8 .7-11.9s-.2-8-.7-11.9l94.1-47C302.6 213.8 326.1 224 352 224z" />
+            </svg>
+          </Button>
+        </div>
 
         <div className="flex flex-col lg:flex-row gap-2 lg:gap-6">
           <Button
             className="flex-1"
+            onClick={nextAdjective}
             disabled={isWaitingBuffer}
-            onClick={() => nextAdjective()}
           >
             Novo adjetivo
           </Button>
@@ -129,7 +156,7 @@ function Button({ children, className, ...props }) {
     <button
       className={classNames(
         className,
-        "transition-all text-white hover:text-slate-900 border-white hover:bg-cyan-400 focus:bg-cyan-400 text-sm lg:text-lg border border-transparent hover:border-cyan-300 px-4 py-2 rounded"
+        "transition-all text-white hover:text-slate-900 border border-white hover:bg-cyan-400 focus:bg-cyan-400 text-sm lg:text-lg  hover:border-cyan-300 px-4 py-2 rounded"
       )}
       {...props}
     >
